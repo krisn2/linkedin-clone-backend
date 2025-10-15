50a7b1048f06d538f2395d3071c20bb399f1db05
krishna sarone <167612353+krisn2@users.noreply.github.com>
2025-10-15 16:49:55 +0530

    fix

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ const server = http.createServer(app);
 
 const io = new Server(server, {
   cors: {
-    origin: ["http://localhost:5173", "https://linkedin-clone-frontend-one.vercel.app/"],
+    origin:"*",
     methods: ["GET", "POST","PUT", "DELETE"],
   },
 });
